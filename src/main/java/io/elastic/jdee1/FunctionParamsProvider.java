598e5f4f419a85e4f2933fd52f07d2c3a3e3f06f
Kirill <kyrylo.levytskyi@elastic.io>
2018-08-16 16:06:04 +0300

    no message

--- src/main/java/io/elastic/jdee1/FunctionParamsProvider.java
+++ src/main/java/io/elastic/jdee1/FunctionParamsProvider.java
@@ -31,6 +31,7 @@ public class FunctionParamsProvider implements DynamicMetadataProvider, SelectMo
   public JsonObject getMetaModel(JsonObject configuration) {
     JsonObject result = Json.createObjectBuilder().build();
     JsonObject inMetadata = Json.createObjectBuilder().build();
+    logger.info("configuration: {}", configuration);
     JsonObject properties = getColumns(configuration);
     inMetadata = Json.createObjectBuilder().add("type", "object")
         .add("properties", properties).build();
