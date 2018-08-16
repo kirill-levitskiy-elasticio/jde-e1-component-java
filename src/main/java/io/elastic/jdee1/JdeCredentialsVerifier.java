3a7793ad9d331b2f7058ca4587b658dd9cb99b5e
Kirill <kyrylo.levytskyi@elastic.io>
2018-08-16 09:26:33 +0300

    no message

--- src/main/java/io/elastic/jdee1/JdeCredentialsVerifier.java
+++ src/main/java/io/elastic/jdee1/JdeCredentialsVerifier.java
@@ -1,5 +1,6 @@
 package io.elastic.jdee1;
 
+import io.elastic.api.CredentialsVerifier;
 import io.elastic.api.InvalidCredentialsException;
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -7,15 +8,17 @@ import javax.json.JsonObjectBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JdeCredentialsVerifier implements io.elastic.api.CredentialsVerifier {
+public class JdeCredentialsVerifier implements CredentialsVerifier {
 
   private static final Logger logger = LoggerFactory.getLogger(JdeCredentialsVerifier.class);
 
   @Override
   public void verify(JsonObject configuration) throws InvalidCredentialsException {
+    logger.info("About to verify provided JDE E1 Credentials");
+
     Utils jdeinstance = new Utils();
     final JsonObjectBuilder result = Json.createObjectBuilder();
-
+/*
     try {
       result.add("result", jdeinstance.getTemplate_actionPerformed(configuration));
     } catch (Exception e) {
@@ -27,6 +30,7 @@ public class JdeCredentialsVerifier implements io.elastic.api.CredentialsVerifie
         logger.error("Failed to closed database connection");
       }
     }
+    */
   }
 
 }
